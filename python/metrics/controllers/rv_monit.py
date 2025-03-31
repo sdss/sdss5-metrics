e6158e5493d01641dfab15e22fc3d5d349e0cb9c
johndonor3 <j.donor@tcu.edu>
2025-03-31 17:41:20 +0000

    mjd cut in rv monit

--- python/metrics/controllers/rv_monit.py
+++ python/metrics/controllers/rv_monit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env/python
 
-from quart import render_template, Blueprint
+from quart import render_template, Blueprint, request
 
 import numpy as np
 
@@ -17,12 +17,22 @@ rv_monit_page = Blueprint("rv_monit_page", __name__)
 async def index():
     templateDict = getTemplateDictBase()
 
+    mjd_start = "none"
+    if request.args:
+        try:
+            mjd_start = int(request.args["mjd_start"])
+        except ValueError:
+            mjd_start = "none"
+
     field_mjds, delta_nom = await wrapBlocking(mjd_by_cadence, "bright_single_18x1_v2")
 
     fields = dict()
     for f in field_mjds:
         fid = f["field_id"]
         dates = f["mjd"]
+        if mjd_start != "none":
+            if np.min(dates) < mjd_start:
+                continue
         deltas = list(np.sort(dates - np.min(dates)))
         fields[fid] = [[i, d] for i, d in enumerate(deltas)]
     
@@ -30,7 +40,8 @@ async def index():
 
     templateDict.update({
         "fields": fields,
-        "goal": goal
+        "goal": goal,
+        "mjd_start": mjd_start
         })
 
     return await render_template("rv_monit.html", **templateDict)
